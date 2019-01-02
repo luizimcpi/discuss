fef5365a355fd1c8c20858996f0d5b5c88d3e05d
Luiz Henrique <luiz.hse@hotmail.com>
2019-01-02 15:08:34 -0200

    Creating new topics forms /topics/new

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,15 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
+
+    alias Discuss.Topic
     
     def new(conn, params) do
-        IO.puts "new function has been called"
+        changeset = Topic.changeset(%Topic{}, %{})
+
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, params) do
+        
     end
 end
